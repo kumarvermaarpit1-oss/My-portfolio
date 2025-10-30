cd8a4ef8f8a9b9605f9ba2f3aa678db63dd56473
kumarvermaarpit1-oss <kumarvermaarpit1@gmail.com>
2025-10-30 07:51:20 -0700

    added Arpit portfolio

---
+++ Arpit portfolio/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.contact-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Contact form feature coming soon!');
+    });
+});
+
+document.querySelector('.download-btn').addEventListener('click', () => {
+    alert('Download CV feature coming soon!');
+});
